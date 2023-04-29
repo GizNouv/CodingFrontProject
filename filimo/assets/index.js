ab79ca3667d5377f29011cb63b41738f15051a26
GizNouv <giznouv@gmail.com>
2023-04-29 17:09:27 +0430

    first commit

---
+++ filimo/assets/index.js
@@ -0,0 +1,16 @@
+var element1 = document.getElementById("box1");
+var element2 = document.getElementById("box2");
+var element3 = document.getElementById("box3");
+var element4 = document.getElementById("box4");
+function expand1(){
+    element1.classList.toggle("active");
+}
+function expand2(){
+    element2.classList.toggle("active");
+}
+function expand3(){
+    element3.classList.toggle("active");
+}
+function expand4(){
+    element4.classList.toggle("active");
+}
